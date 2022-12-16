c610c6b397379876e9a1dc29928a5c442f5e3e0b
jonathanmunamire <jonathanmunamire@gmail.com>
2022-12-16 16:30:01 +0200

    Add Book Section

--- src/components/book.js
+++ src/components/book.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
+import './book.css';
 
 const Book = ({
   title, author, id, category,
@@ -15,28 +16,32 @@ const Book = ({
     <section>
       <div className="single-book">
         <div className="section-one">
-          <div>
-            <p>{category}</p>
-            <p>{title}</p>
-            <p>{author}</p>
+          <div className="one-one">
+            <p className="p category">{category}</p>
+            <p className="p title">{title}</p>
+            <p className="p author">{author}</p>
           </div>
-          <div>
+          <div className="one-two">
             <button type="button">Comments</button>
+            <div className="vertical-line-one" />
             <button type="button" onClick={deleted}>Remove</button>
+            <div className="vertical-line-one" />
             <button type="button">Edit</button>
           </div>
         </div>
-        <div className="section-two">
-          <div>icon</div>
-          <div>
-            <p>64%</p>
-            <p>Completed</p>
+        <div className="section-two-three">
+          <div className="section-two">
+            <p className="round" />
+            <div className="two-two">
+              <p className="percentage">75%</p>
+              <p className="completed">Completed</p>
+            </div>
           </div>
-        </div>
-        <div className="section-three">
-          <p>CURRENT CHAPTER</p>
-          <p>Chapter 17</p>
-          <button type="button">UPDATE PROGRESS</button>
+          <div className="section-three">
+            <p>CURRENT CHAPTER</p>
+            <p>Chapter 17</p>
+            <button type="button">UPDATE PROGRESS</button>
+          </div>
         </div>
       </div>
     </section>
