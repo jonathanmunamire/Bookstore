9b5de4dcf1dee8ff3f595e581d083ace72175d15
jonathanmunamire <jonathanmunamire@gmail.com>
2022-12-16 12:18:04 +0200

    Style Nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const NavBar = () => (
   <header>
-    <h1>Bookstore CMS</h1>
-    <nav>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
-    </nav>
+    <div className="nav-one">
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </nav>
+    </div>
+    <div className="nav-two">
+      <button type="button">icon</button>
+    </div>
+
   </header>
 );
 
